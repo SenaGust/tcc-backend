69bfbe79152925c81703524750c35ed0d2a18911
SenaGust <gustavosena98@live.com>
2021-09-25 20:35:52 -0300

    Added cors configuration

---
+++ src/main/java/com/tcc/backend/config/cors/CorsConfiguration.java
@@ -0,0 +1,23 @@
+package com.tcc.backend.config.cors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class CorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*")
+                        .allowedOrigins("http://localhost:3000/", "https://womens-map-dev.herokuapp.com/", "https://womens-map.herokuapp.com/")
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
