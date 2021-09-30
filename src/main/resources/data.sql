de7954e5a8471a5c1ec4c1efc1356e28c27f966d
SenaGust <gustavosena98@live.com>
2021-09-29 21:57:46 -0300

    added initial values

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+delete from occurrence_type;
+INSERT INTO occurrence_type (id,name,description,weight) VALUES (1, 'Iluminação', 'Iluminação baixa ou inexistente', 1);
+INSERT INTO occurrence_type (id,name,description,weight) VALUES (2, 'Assédio Sexual', 'Assédio Sexual', 1);
+INSERT INTO occurrence_type (id,name,description,weight) VALUES (3, 'Morte', 'Morte', 1);
+INSERT INTO occurrence_type (id,name,description,weight) VALUES (4, 'Assalto', 'Assalto', 1);
