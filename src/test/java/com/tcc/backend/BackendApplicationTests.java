adcd49834f139540ddfc4dde44fa1a3557781dc2
SenaGust <gustavosena98@live.com>
2021-10-10 23:41:03 -0300

    Testing prod

--- src/test/java/com/tcc/backend/BackendApplicationTests.java
+++ src/test/java/com/tcc/backend/BackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.tcc.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class BackendApplicationTests {
 
     @Test
