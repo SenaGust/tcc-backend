f0141dbb34dfbcf318bf2eebe2944a76fd77d118
SenaGust <gustavosena98@live.com>
2021-10-27 20:03:53 -0300

    trying to improve analysis

--- src/main/java/com/tcc/analisys/clusterer/OccurrenceClusterer.java
+++ src/main/java/com/tcc/analisys/clusterer/OccurrenceClusterer.java
@@ -2,10 +2,11 @@ package com.tcc.analisys.clusterer;
 
 import com.tcc.analisys.clusterer.models.OccurrenceClusteredDTO;
 import com.tcc.analisys.clusterer.models.OccurrenceInstance;
-import net.sf.javaml.clustering.IterativeKMeans;
-import net.sf.javaml.clustering.evaluation.SumOfSquaredErrors;
+import net.sf.javaml.clustering.IterativeMultiKMeans;
+import net.sf.javaml.clustering.evaluation.BICScore;
 import net.sf.javaml.core.Dataset;
 import net.sf.javaml.core.DefaultDataset;
+import net.sf.javaml.distance.ManhattanDistance;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,10 +17,16 @@ public class OccurrenceClusterer {
         Dataset dataset = new DefaultDataset();
         occurrenceInstances.stream().forEach(occurrenceInstance -> dataset.add(occurrenceInstance.toSparseInstance()));
 
-        IterativeKMeans iterativeKMeans = new IterativeKMeans(3, occurrenceInstances.size() - 1, new SumOfSquaredErrors());
+
+        IterativeMultiKMeans iterativeKMeans = new IterativeMultiKMeans(3, generateKMax(occurrenceInstances.size()), 100, 100, new ManhattanDistance(), new BICScore());
         Dataset[] clusters = iterativeKMeans.cluster(dataset);
 
 
         return Arrays.stream(clusters).map(cluster -> new OccurrenceClusteredDTO(cluster)).collect(Collectors.toList());
+    }
+
+
+    private int generateKMax(int totalItems) {
+        return Math.max(totalItems / 3, 3);
     }
 }
