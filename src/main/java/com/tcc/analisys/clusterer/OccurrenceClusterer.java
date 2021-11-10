b37dac3586290813d774e491ce8678a763461926
SenaGust <gustavosena98@live.com>
2021-11-09 23:41:11 -0300

    Trying to improve dbscan

--- src/main/java/com/tcc/analisys/clusterer/OccurrenceClusterer.java
+++ src/main/java/com/tcc/analisys/clusterer/OccurrenceClusterer.java
@@ -2,7 +2,8 @@ package com.tcc.analisys.clusterer;
 
 import com.tcc.analisys.clusterer.models.OccurrenceClusteredDTO;
 import com.tcc.analisys.clusterer.models.OccurrenceInstance;
-import net.sf.javaml.clustering.DensityBasedSpatialClustering;
+import net.sf.javaml.clustering.IterativeMultiKMeans;
+import net.sf.javaml.clustering.evaluation.SumOfSquaredErrors;
 import net.sf.javaml.core.Dataset;
 import net.sf.javaml.core.DefaultDataset;
 import net.sf.javaml.distance.ManhattanDistance;
@@ -16,13 +17,8 @@ public class OccurrenceClusterer {
         Dataset dataset = new DefaultDataset();
         occurrenceInstances.stream().forEach(occurrenceInstance -> dataset.add(occurrenceInstance.toSparseInstance()));
 
-        // IterativeMultiKMeans iterativeKMeans = new IterativeMultiKMeans(3, generateKMax(occurrenceInstances.size()), 100, 100, new ManhattanDistance(), new BICScore());
-        // Dataset[] clusters = iterativeKMeans.cluster(dataset);
-
-        DensityBasedSpatialClustering DBSCAN = new DensityBasedSpatialClustering( 0.01, 2, new ManhattanDistance());
-        Dataset[] clusters = DBSCAN.cluster(dataset);
-
-
+        IterativeMultiKMeans iterativeKMeans = new IterativeMultiKMeans(3, generateKMax(occurrenceInstances.size()), 100, 100, new ManhattanDistance(), new SumOfSquaredErrors());
+        Dataset[] clusters = iterativeKMeans.cluster(dataset);
 
         return Arrays.stream(clusters).map(cluster -> new OccurrenceClusteredDTO(cluster)).collect(Collectors.toList());
     }
