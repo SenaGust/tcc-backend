10928811da6b6af23a88c24e05fc925ac77bc74e
SenaGust <gustavosena98@live.com>
2021-11-09 21:59:41 -0300

    Trying to improve dbscan

--- src/main/java/com/tcc/analisys/clusterer/OccurrenceClusterer.java
+++ src/main/java/com/tcc/analisys/clusterer/OccurrenceClusterer.java
@@ -5,6 +5,7 @@ import com.tcc.analisys.clusterer.models.OccurrenceInstance;
 import net.sf.javaml.clustering.DensityBasedSpatialClustering;
 import net.sf.javaml.core.Dataset;
 import net.sf.javaml.core.DefaultDataset;
+import net.sf.javaml.distance.ManhattanDistance;
 
 import java.util.Arrays;
 import java.util.List;
@@ -14,11 +15,11 @@ public class OccurrenceClusterer {
     public List<OccurrenceClusteredDTO> run(List<OccurrenceInstance> occurrenceInstances) {
         Dataset dataset = new DefaultDataset();
         occurrenceInstances.stream().forEach(occurrenceInstance -> dataset.add(occurrenceInstance.toSparseInstance()));
-        
+
         // IterativeMultiKMeans iterativeKMeans = new IterativeMultiKMeans(3, generateKMax(occurrenceInstances.size()), 100, 100, new ManhattanDistance(), new BICScore());
         // Dataset[] clusters = iterativeKMeans.cluster(dataset);
 
-        DensityBasedSpatialClustering DBSCAN = new DensityBasedSpatialClustering();
+        DensityBasedSpatialClustering DBSCAN = new DensityBasedSpatialClustering( 0.15, 3, new ManhattanDistance());
         Dataset[] clusters = DBSCAN.cluster(dataset);
 
         return Arrays.stream(clusters).map(cluster -> new OccurrenceClusteredDTO(cluster)).collect(Collectors.toList());
